bc62f5ab2a3f45a95f7c286d2fa2435bc4793b28
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2024-04-11 12:19:34 -0400

    Make Vite Template Installable Add the vite-pwa plugin and configuartion to the default vite-react-ts template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Team 9072 Scouting App
+
+This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "prompt",
+      injectRegister: "auto",
+      pwaAssets: {
+        config: "pwa-assets.config.ts",
+      },
+      manifest: {
+        name: "Team 9072 Scouting App",
+        short_name: "9072Scouting",
+        description: "A FRC scouting app for team 9072",
+        orientation: "portrait",
+        display_override: ["fullscreen", "standalone"],
+        display: "fullscreen",
+        theme_color: "#242424",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
